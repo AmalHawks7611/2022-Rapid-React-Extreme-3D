40385c2f52f621b8bc16ee13dfd0838c6494d73a
sensoyg <gokhansensoy25@gmail.com>
2022-02-17 16:11:57 +0300

    init commit

---
+++ AmalHawks7611_RobotCode/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,189 @@
+package frc.robot;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Tanjant;
+
+public class Robot extends TimedRobot {
+  private static Victor intake = new Victor(1);
+  private static Victor palet = new Victor(2);
+  private static Victor atis = new Victor(3);
+  private static Victor sol_motor = new Victor(4);
+  private static Victor sag_motor = new Victor(5);
+  private static DifferentialDrive robot_drive = new DifferentialDrive(sol_motor, sag_motor);
+  private static Joystick joystick = new Joystick(0);
+  NetworkTableInstance photon = NetworkTableInstance.create();
+  NetworkTable table = photon.getTable("photonvision").getSubTable("camera");
+  private int atis_boolean = 0;
+  private int intake_boolean = 0;
+  private double palet_double = 0.0;
+  private double xSpeed = 0.0;
+  PIDController pid = new PIDController(0.1, 0, 0.0);
+  private double arcade_x = 0.0;
+  private double arcade_y = 0.0;
+  private final Timer timer = new Timer();
+  
+  @Override
+  public void robotInit() {
+    photon.startClient("10.76.11.102");
+  }
+
+  @Override
+  public void robotPeriodic() {
+    
+  }
+  @Override
+  public void autonomousInit() {
+    timer.reset();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    double range = olimpiyat_zubeyir_teoremi(getPitch(), 0.6, 0.9);
+    double yaw = getYaw();
+    atis.set(1);
+    robot_drive.arcadeDrive(arcade_x, arcade_y);
+    //System.out.println("Yaw : " + yaw);
+      if(hasTarget()){
+        System.out.println(range);
+        if(yaw > 10){
+          arcade_x = -0.45;
+          arcade_y = 0.4;
+        }
+        else if(yaw < -10){
+          arcade_x = 0.4;
+          arcade_y = 0.4;
+        }
+        else{
+          arcade_x = 0;
+          if(range > 2.2){
+            arcade_y = 0.4;
+          }
+          else if(range < 1.85){
+            arcade_y = -0.4;
+          }
+          else{
+            arcade_y = 0.0;
+            timer.start();
+            
+            if(timer.get() < 1.0)
+              palet.set(1);
+            if(timer.get() > 1.0 && timer.get() < 1.4)
+              palet.set(0);
+            if(timer.get() > 2.0)
+              palet.set(-1);
+          }
+        }
+      }
+      else
+        robot_drive.arcadeDrive(0, 0);
+  }
+
+  @Override
+  public void teleopInit() {
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+    robot_drive.arcadeDrive(joystick.getX() * 0.9, joystick.getY()* -1 * 0.9);
+    atis.set(atis_boolean);
+    intake.set(intake_boolean);
+    palet.set(palet_double);
+
+    if(joystick.getRawButtonReleased(1)){
+      if(atis_boolean == 0){
+        atis_boolean = 1;
+      }
+      else if(atis_boolean == 1){
+        atis_boolean = 0;
+      }
+      else{
+        atis_boolean = 0;
+      }
+    }
+    if(joystick.getRawButtonReleased(2)){
+      if(intake_boolean == 0){
+        intake_boolean = 1;
+      }
+      else if(intake_boolean == 1){
+       intake_boolean = 0;
+      }
+      else{
+        intake_boolean = 0;
+      }
+    }
+    if(joystick.getRawButton(6)){
+      palet_double = 1;
+    }
+    else if(joystick.getRawButton(5)){
+      palet_double = -1;
+    }
+    else{
+      palet_double = 0;
+    }
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {
+    robot_drive.arcadeDrive(0, 0.5);
+  }
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+  
+  public boolean hasTarget(){
+    NetworkTableEntry result = table.getEntry("hasTarget");
+    return result.getBoolean(false);
+  }
+  public double getYaw(){
+    NetworkTableEntry result = table.getEntry("targetYaw");
+    return result.getDouble(0);
+  }
+  public double getPitch(){
+    NetworkTableEntry result = table.getEntry("targetPitch");
+    return result.getDouble(0);
+  }
+  public double olimpiyat_zubeyir_teoremi(double pitch, double cam_height, double target_height){
+    double max = Math.max(cam_height, target_height);
+    double min = Math.min(cam_height, target_height);
+    pitch = mutlak_samet(pitch);
+    pitch = Math.round(pitch);
+    double result = (max-min) * Tanjant.tan(90 - (int)pitch);
+    return result;
+  }
+  public double mutlak_samet(double deger){
+    if(deger > 0){
+      return deger;
+    }
+    else{
+      return deger * -1;
+    }
+  }
+
+
+
+}
---
+++ AmalHawks7611_RobotCode/src/main/java/frc/robot/Tanjant.java
@@ -0,0 +1,54 @@
+package frc.robot;
+public class Tanjant {
+    
+    public static double tan(int x){
+        if(x == 89) return 57.289;
+        else if(x == 88) return 28.636;
+        else if(x == 87) return 19.081;
+        else if(x == 86) return 14.300;
+        else if(x == 85) return 11.430;
+        else if(x == 84) return 9.514;
+        else if(x == 83) return 8.144;
+        else if(x == 82) return 7.115;
+        else if(x == 81) return 6.313;
+        else if(x == 80) return 5.671;
+        else if(x == 79) return 5.144;
+        else if(x == 78) return 4.704;
+        else if(x == 77) return 4.331;
+        else if(x == 76) return 4.010;
+        else if(x == 75) return 3.732;
+        else if(x == 74) return 3.487;
+        else if(x == 73) return 3.270;
+        else if(x == 72) return 3.077;
+        else if(x == 71) return 2.904;
+        else if(x == 70) return 2.747;  
+        else if(x == 69) return 2.605;
+        else if(x == 68) return 2.475;  
+        else if(x == 67) return 2.355;
+        else if(x == 66) return 2.246;
+        else if(x == 65) return 2.144;
+        else if(x == 64) return 2.050;
+        else if(x == 63) return 1.962;
+        else if(x == 62) return 1.880;
+        else if(x == 61) return 1.804;
+        else if(x == 60) return 1.732;
+        else if(x == 59) return 1.664;
+        else if(x == 58) return 1.600;
+        else if(x == 57) return 1.539;
+        else if(x == 56) return 1.482;
+        else if(x == 55) return 1.428;
+        else if(x == 54) return 1.376;
+        else if(x == 53) return 1.327;
+        else if(x == 52) return 1.279;
+        else if(x == 51) return 1.234;
+        else if(x == 50) return 1.191;
+        else if(x == 49) return 1.150;
+        else if(x == 48) return 1.110;
+        else if(x == 47) return 1.072;
+        else if(x == 46) return 1.035;
+        else if(x == 45) return 1.0;
+        else return 0;
+    }
+    
+
+}
