5400f3ba3147a6f446ded7550bfafe5da3f97af8
sensoyg <gokhansensoy25@gmail.com>
2022-02-18 17:56:59 +0300

    major changes

--- AmalHawks7611_RobotCode/src/main/java/frc/robot/Robot.java
+++ AmalHawks7611_RobotCode/src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import org.photonvision.PhotonUtils;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -21,14 +23,18 @@ public class Robot extends TimedRobot {
   private static Victor sag_motor = new Victor(5);
   private static DifferentialDrive robot_drive = new DifferentialDrive(sol_motor, sag_motor);
   private static Joystick joystick = new Joystick(0);
-  NetworkTableInstance photon = NetworkTableInstance.create();
-  NetworkTable table = photon.getTable("photonvision").getSubTable("camera");
+  private static NetworkTableInstance photon = NetworkTableInstance.create();
+  private static NetworkTable table = photon.getTable("photonvision").getSubTable("camera");
   private int atis_boolean = 0;
   private int intake_boolean = 0;
-  private double palet_double = 0.0;
-  private double arcade_x = 0.0;
-  private double arcade_y = 0.0;
-  private final Timer timer = new Timer();
+  private static double palet_double = 0.0;
+  private static double arcade_x = 0.0;
+  private static double arcade_y = 0.0;
+  private static double arcade_x_teleop = 0.0;
+  private static double arcade_y_teleop = 0.0;
+  private static Timer timer = new Timer();
+  double palet_status = 0.0;
+  double atis_status = 0.0;
   
   @Override
   public void robotInit() {
@@ -41,63 +47,43 @@ public class Robot extends TimedRobot {
   }
   @Override
   public void autonomousInit() {
-    timer.reset();
   }
 
   @Override
   public void autonomousPeriodic() {
-    double range = olimpiyat_zubeyir_teoremi(getPitch(), 0.6, 0.9);
-    double yaw = getYaw();
+    //e = olimpiyat_zubeyir_teoremi(getPitch(), 0.67, 2.5, 40);
+    //double yaw = getYaw();
     atis.set(1);
+    timer.reset();
+    align_robot(arcade_x, arcade_y);
+    distance_set_robot();
     robot_drive.arcadeDrive(arcade_x, arcade_y);
-      if(hasTarget()){
-        System.out.println(range);
-        if(yaw > 10){
-          arcade_x = -0.45;
-          arcade_y = 0.4;
-        }
-        else if(yaw < -10){
-          arcade_x = 0.4;
-          arcade_y = 0.4;
-        }
-        else{
-          arcade_x = 0;
-          if(range > 2.2){
-            arcade_y = 0.4;
-          }
-          else if(range < 1.85){
-            arcade_y = -0.4;
-          }
-          else{
-            arcade_y = 0.0;
-            timer.start();
-            
-            if(timer.get() < 1.0)
-              palet.set(1);
-            if(timer.get() > 1.0 && timer.get() < 1.4)
-              palet.set(0);
-            if(timer.get() > 2.0)
-              palet.set(-1);
-          }
-        }
-      }
-      else
-        robot_drive.arcadeDrive(0, 0);
   }
 
   @Override
   public void teleopInit() {
     timer.reset();
-    timer.start();
   }
 
   @Override
   public void teleopPeriodic() {
-
-    robot_drive.arcadeDrive(joystick.getX() * 0.9, joystick.getY()* -1 * 0.9);
+    
+    //robot_drive.arcadeDrive(joystick.getX() * 0.9, joystick.getY()* -1 * 0.9);
+    robot_drive.arcadeDrive(arcade_x_teleop, arcade_y_teleop);
     atis.set(atis_boolean);
     intake.set(intake_boolean);
     palet.set(palet_double);
+
+    if(joystick.getRawButton(10)){
+      align_robot(arcade_x_teleop, arcade_y_teleop);
+    }
+    else{
+      arcade_y_teleop = joystick.getY() * -1;
+      arcade_x_teleop = joystick.getX();
+    }
+    if(joystick.getRawButton(7)){
+      atis.set(-0.6);
+    }
 
     if(joystick.getRawButtonReleased(1)){
       if(atis_boolean == 0){
@@ -143,7 +129,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {
-    robot_drive.arcadeDrive(0, 0.5);
+    robot_drive.arcadeDrive(-0.6, 0.4);
   }
 
   @Override
@@ -152,35 +138,66 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationPeriodic() {}
   
-  public boolean hasTarget(){
+  public static boolean hasTarget(){
     NetworkTableEntry result = table.getEntry("hasTarget");
     return result.getBoolean(false);
   }
-  public double getYaw(){
+  public static double getYaw(){
     NetworkTableEntry result = table.getEntry("targetYaw");
     return result.getDouble(0);
   }
-  public double getPitch(){
+  public static double getPitch(){
     NetworkTableEntry result = table.getEntry("targetPitch");
     return result.getDouble(0);
   }
-  public double olimpiyat_zubeyir_teoremi(double pitch, double cam_height, double target_height){
-    double max = Math.max(cam_height, target_height);
-    double min = Math.min(cam_height, target_height);
-    pitch = mutlak_samet(pitch);
+  public static double olimpiyat_zubeyir_teoremi(double pitch, double cam_height, double target_height, double cam_pitch){
     pitch = Math.round(pitch);
-    double result = (max-min) * Tanjant.tan(90 - (int)pitch);
+    //double result = (max-min) * Tanjant.tan(90 - ((int)pitch) + 45);
+    double result = (target_height - cam_height) / Tanjant.tan(((int)pitch) + (int)cam_pitch);
     return result;
   }
   public double mutlak_samet(double deger){
-    if(deger > 0){
+    if(deger > 0)
       return deger;
-    }
-    else{
+    else
       return deger * -1;
+  }
+  public static void distance_set_robot(){
+    if(hasTarget()){
+      double pitch_teleop = getPitch();
+      double range = olimpiyat_zubeyir_teoremi(pitch_teleop, 0.67, 2.5, 40);
+      if(range > 2.00){
+        arcade_y = 0.4;
+      }
+      else if(range < 1.80){
+        arcade_y = -0.4;
+      }
+      else{
+        arcade_y = 0.0;
+        timer.start();
+        if(timer.get() > 1.0 && timer.get() < 1.4)
+          palet.set(0);
+        if(timer.get() > 2.0)
+          palet.set(-1);
+      }
+      
     }
   }
-
-
-
+  public static void align_robot(double arcade_x, double arcade_y){
+    if(hasTarget()){
+    double yaw_teleop = getYaw();
+    if(yaw_teleop > 3){
+      arcade_x = 0.4;
+      arcade_y = 0.4;
+    }
+    else if(yaw_teleop < -3){
+      arcade_x = -0.4;
+      arcade_y = 0.4;
+    }
+    else{
+      arcade_x = 0;
+      arcade_y = 0;
+    }
+  }
+  }
 }
--- AmalHawks7611_RobotCode/src/main/java/frc/robot/Tanjant.java
+++ AmalHawks7611_RobotCode/src/main/java/frc/robot/Tanjant.java
@@ -23,7 +23,7 @@ public class Tanjant {
         else if(x == 71) return 2.904;
         else if(x == 70) return 2.747;  
         else if(x == 69) return 2.605;
-        else if(x == 68) return 2.475;  
+        else if(x == 68) return 2.475;
         else if(x == 67) return 2.355;
         else if(x == 66) return 2.246;
         else if(x == 65) return 2.144;
@@ -46,7 +46,29 @@ public class Tanjant {
         else if(x == 48) return 1.110;
         else if(x == 47) return 1.072;
         else if(x == 46) return 1.035;
-        else if(x == 45) return 1.0;
+        else if(x == 45) return 1.000;
+        else if(x == 44) return 0.966;
+        else if(x == 43) return 0.932;
+        else if(x == 42) return 0.900;
+        else if(x == 41) return 0.869;
+        else if(x == 40) return 0.839;
+        else if(x == 39) return 0.809;
+        else if(x == 38) return 0.781;
+        else if(x == 37) return 0.753;
+        else if(x == 36) return 0.726;
+        else if(x == 35) return 0.700;
+        else if(x == 34) return 0.674;
+        else if(x == 33) return 0.649;
+        else if(x == 32) return 0.624;
+        else if(x == 31) return 0.600;
+        else if(x == 30) return 0.577;
+        else if(x == 29) return 0.554;
+        else if(x == 28) return 0.531;
+        else if(x == 27) return 0.509;
+        else if(x == 26) return 0.487;
+        else if(x == 25) return 0.466;
+        else if(x == 24) return 0.445;
+        else if(x == 23) return 0.424;
         else return 0;
     }
     
