5c6daeb7b606dc1177e106ca15f4dee00116fe26
sensoyg <gokhansensoy25@gmail.com>
2022-03-02 16:04:46 +0300

    major changes / controls, autonomous etc.

--- AmalHawks7611_RobotCode/src/main/java/frc/robot/Robot.java
+++ AmalHawks7611_RobotCode/src/main/java/frc/robot/Robot.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -18,6 +23,7 @@ public class Robot extends TimedRobot {
   private static Victor atis = new Victor(3);
   private static Victor sol_motor = new Victor(4);
   private static Victor sag_motor = new Victor(5);
+  private static Victor target_led = new Victor(7);
   private static DifferentialDrive robot_drive = new DifferentialDrive(sol_motor, sag_motor);
   private static Joystick joystick = new Joystick(0);
   private static NetworkTableInstance photon = NetworkTableInstance.create();
@@ -33,18 +39,26 @@ public class Robot extends TimedRobot {
   private static double CAM_HEIGHT = 0.67;
   private static double TARGET_HEIGHT = 2.75;
   private static double CAM_PITCH = 53.0;
-  private static double TARGET_DISTANCE = 1.5;
+  private static double TARGET_DISTANCE = 2.0;
   private double atis_devir = 0.9;
   private double direction_boolean = 1.0;
+  private static boolean isAligned = false;
   
   @Override
   public void robotInit() {
     photon.startClient("10.76.11.102");
-
+    UsbCamera camera = CameraServer.startAutomaticCapture(0);
+    camera.setResolution(640, 480);
   }
 
   @Override
   public void robotPeriodic() {
+    if(hasTarget()){
+      target_led.set(1);
+    }
+    else{
+      target_led.set(0);
+    }
   }
 
   @Override
@@ -56,6 +70,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     timer.reset();
+    timer.start();
   }
 
   @Override
@@ -63,23 +78,27 @@ public class Robot extends TimedRobot {
     robot_drive.arcadeDrive(arcade_x, arcade_y);
     double range = olimpiyat_zubeyir_teoremi(CAM_HEIGHT, TARGET_HEIGHT, CAM_PITCH);
     System.out.println(range);
-    atis.set(1);
+    atis.set(atis_devir);
+    arcade_y = -0.7;
+    if(hasTarget()){
+      arcade_y = 0;
+    }
     align_robot();
-    distance_set_auto(range, TARGET_DISTANCE);
+    if(isAligned)
+      distance_set_auto(range, TARGET_DISTANCE);
   }
-
   @Override
   public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {
     robot_drive.arcadeDrive(arcade_x, arcade_y);
-    atis.set(atis_double);
+    //atis.set(atis_double);
     intake.set(intake_double);
     palet.set(palet_double);
     actuator.set(actuator_double);
     samet_actuator.set(samet_actuator_double);
-
+    System.out.println(joystick.getPOV());
     if(joystick.getRawButtonReleased(8)){
       if(actuator_double == 0){
         actuator_double = 1.0;
@@ -88,7 +107,7 @@ public class Robot extends TimedRobot {
         actuator_double = 0;
       }
     }
-    if(joystick.getRawButtonReleased(11)){
+    if(joystick.getRawButtonReleased(7)){
       if(samet_actuator_double == 1.0){
         samet_actuator_double = -1.0;
       }
@@ -97,8 +116,16 @@ public class Robot extends TimedRobot {
       }
     }
 
-    if(joystick.getRawButton(10)){
+    if(joystick.getRawButton(12)){
       align_robot();
+    }
+    else if(joystick.getPOV() <= 315 && joystick.getPOV() >= 215){
+      arcade_x = -0.45;
+      arcade_y = 0.3;
+    }
+    else if(joystick.getPOV() <= 135 && joystick.getPOV() >= 45){
+      arcade_x = 0.45;
+      arcade_y = 0.3;
     }
     else{
       if(slider_position() >= 0.5){
@@ -119,11 +146,17 @@ public class Robot extends TimedRobot {
         direction_boolean = 1.0;
     }
     
-    if(joystick.getRawButton(7)){
-      atis.set(-1);
+    if(joystick.getRawButton(11)){
+      atis.set(-1 * atis_devir);
     }
 
-    if(joystick.getRawButtonReleased(1)){
+    else if(joystick.getRawButton(1)){
+      atis.set(atis_devir);
+    }
+    else{
+      atis.set(0);
+    }
+    /*if(joystick.getRawButtonReleased(1)){
       if(atis_double == 0){
         atis_double = atis_devir;
       }
@@ -133,7 +166,7 @@ public class Robot extends TimedRobot {
       else{
         atis_double = 0;
       }
-    }
+    }*/
     if(joystick.getRawButtonReleased(2)){
       if(intake_double == 0){
         intake_double = 1;
@@ -194,8 +227,11 @@ public class Robot extends TimedRobot {
         palet.set(1);
         if(timer.get() > 0.8)
           palet.set(0);
-        if(timer.get() > 2.0)
+        if(timer.get() > 3.0){
           palet.set(-1);
+        }
+
+         
       }
       
     }
@@ -205,15 +241,18 @@ public class Robot extends TimedRobot {
   public static void align_robot(){
     if(hasTarget()){
     double yaw_teleop = getYaw();
-    if(yaw_teleop > 3){
+    if(yaw_teleop > 1){
+      isAligned = false;
       arcade_x = 0.45;
       arcade_y = 0.3;
     }
-    else if(yaw_teleop < -3){
+    else if(yaw_teleop < -1){
+      isAligned = false;
       arcade_x = -0.45;
       arcade_y = 0.3;
     }
     else{
+      isAligned = true;
       arcade_x = 0;
       arcade_y = 0;
     }
